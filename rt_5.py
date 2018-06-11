e278eda23a88dac1239189ba83cc0dfd0f360e13
Blue-Swarrow <ayumu.colossians2.6@gmail.com>
2018-06-11 20:49:19 +0900

    change the O/F iteration function

--- rt_5.py
+++ rt_5.py
@@ -26,7 +26,7 @@ class Main:
 #        self.iterat_newton_eta(maxiter, eta_init=0.7)
         self.iterat_brentq_eta(maxiter)
 
-    def iterat_brentq_eta(self, maxiter, eta_min=0.01, eta_max=1.0):
+    def iterat_brentq_eta(self, maxiter, eta_min=0.5, eta_max=2.0):
         """
         Function to calculate O/F using Newton-Raphson method; in this case, using Secant method
         
@@ -45,7 +45,7 @@ class Main:
                 "mp": fuel mass flow rate, [kg/s]
         """
         self.counter_eta_iterat = 1
-        self.eta = optimize.brentq(self.func_Mf, eta_min, eta_max, maxiter=maxiter, xtol=1.0e-3, full_output=False)
+        self.eta = optimize.brentq(self.func_error_Mf, eta_min, eta_max, maxiter=maxiter, xtol=1.0e-3, full_output=False)
         return(self.eta)
 
     def iterat_newton_eta(self, maxiter, eta_init=1.0):
@@ -67,9 +67,51 @@ class Main:
                 "mp": fuel mass flow rate, [kg/s]
         """
         self.counter_eta_iterat = 1
-        self.eta = optimize.newton(self.func_Mf, eta_init, maxiter=maxiter, tol=1.0e-2)
+        self.eta = optimize.newton(self.func_error_Mf, eta_init, maxiter=maxiter, tol=1.0e-2)
         return(self.eta)
 
+    def func_error_Mf(self, eta):
+        """
+        Function to calculate the error of fuel mass consumpiton [%]
+        
+        Parameter
+        ---------
+        eta: float
+            c* efficiency
+
+        Return
+        ---------
+        error: float
+            error of fuel mass consumption [%]
+        """
+        Mf = self.func_Mf(eta)
+        diff_Mf = self.input_param["Mf"] - Mf
+        error = diff_Mf/Mf
+        print("Error of Mf = {} [%]\n\n".format(error))
+        return(error)
+
+    def func_Mf(self, eta):
+        """
+        Function to calculate fuel mass consumption
+        
+        Parameter
+        ---------
+        eta: float
+            c* efficiency
+
+        Return
+        ---------
+        Mf: float
+            Fuel mass consumption [kg]
+        """
+        of = self.iterat_newton_of(eta)
+        mf = np.array(self.ex_df.mox)/of
+        self.anl_df["mf"] = mf
+        self.Mf = integrate.simps(mf, np.array(self.ex_df.index))
+
+        self.counter_eta_iterat += 1
+        return(self.Mf)
+    
 
     def iterat_newton_of(self, eta):
         """
@@ -109,41 +151,76 @@ class Main:
         print("{}".format(self.counter_eta_iterat)+ string + " iteration")
         print("eta = {}".format(eta))
         for i in tqdm(self.ex_df.index):
-            func = lambda of: of - eta*self.ex_df.mox[i]*self.func_cstr(of, self.ex_df.Pc[i]*1.0e-6)[0]
-            tmp = optimize.newton(func, self.of_init[i], maxiter=10, tol=1.0e-5)
+#            tmp = optimize.newton(self.func_error_of, self.of_init[i], maxiter=100, tol=1.0e-5, args=(i, eta))
+#            tmp = optimize.newton(self.func_error_eq14, self.of_init[i], maxiter=100, tol=1.0e-5, args=(i, eta))
+            tmp = optimize.brentq(self.func_error_eq14, 1.0e-6, 100, maxiter=100, xtol=1.0e-5, args=(i, eta))
             of = np.append(of, tmp)
             j +=1
         self.anl_df["of"] = of
-        plt.plot(self.anl_df.of, label="{} interation".format(self.counter_eta_iterat))
+        plt.plot(self.anl_df.of, label="{} iteration".format(self.counter_eta_iterat))
         plt.xlabel("Time [s]")
         plt.ylabel("O/F [-]")
         plt.legend()
         plt.show()
         return(of)
+
     
-    
-    def func_Mf(self, eta):
+    def func_error_of(self, of, t, eta):
+        """ Return the error between assigned O/F and calculated O/F
         """
-        Function to calculate O/F using Newton-Raphson method; in this case, using Secant method
+        of_cal = self.func_of(of, t, eta)
+        diff = of_cal - of
+        error = diff/of_cal
+        return(error)
+
+    def func_of(self, of, t, eta):
+        """ Return: Calculated O/F; float
+        """
+        Pc = self.ex_df.Pc[t]
+        mox = self.ex_df.mox[t]
+        At = np.pi*np.power(self.input_param["Dt"], 2)/4
+        cstr = self.func_cstr(of, Pc*1.0e-6)[0]
+        of = eta*mox*cstr/(Pc*At - mox*eta*cstr)
+        return(of)
+    
+    def func_error_eq14(self, of, t, eta):
+        """ Return the error of Eq.14
+        """
+        left_eq14 = self.func_left_eq14(of,t,eta)
+        right_eq14 = self.func_right_eq14(t)
+        diff = left_eq14 - right_eq14
+        error = diff/right_eq14
+        return(error)
+
+    def func_left_eq14(self, of, t, eta):
+        """
+        Left-hand side value of Eq.(14), Nagata et.al., "Evaluations of Data Reduction Methods for Hybrid Rockets", 65th IAC, 2014.
         
-        Parameter
-        ---------
-        eta: float
-            c* efficiency
+        Return
+        -----------
+        left_val: float
+            eta *cstr_th *(1+1/(O/F)) 
+        """
+        Pc = self.ex_df.Pc[t]
+        cstr = self.func_cstr(of, Pc*1.0e-6)[0]
+        left_val = eta*cstr*(1 + 1/of)
+        return(left_val)
+
+    def func_right_eq14(self, t):
+        """
+        Right-hand side value of Eq.(14), Nagata et.al., "Evaluations of Data Reduction Methods for Hybrid Rockets", 65th IAC, 2014.
 
         Return
-        ---------
-        diff: float
-            difference between fuel consumption obtained by experiment and iterative calculation
+        -----------
+        right_val: float
+            Pc*At/mox 
         """
-        of = self.iterat_newton_of(eta)
-        mf = np.array(self.ex_df.mox)/of
-        self.anl_df["mf"] = mf
-        self.Mf = integrate.simps(mf, np.array(self.ex_df.index))
-        self.diff_Mf = self.Mf - self.input_param["Mf"]
-        print("Difference of Mf = {} [kg]\n\n".format(self.diff_Mf))
-        self.counter_eta_iterat += 1
-        return(self.diff_Mf)
+        Pc = self.ex_df.Pc[t]
+        mox = self.ex_df.mox[t]
+        At = np.pi*np.power(self.input_param["Dt"], 2)/4
+        right_val = Pc*At/mox
+        return(right_val)
+
 
 
 if __name__ == "__main__":
@@ -158,4 +235,65 @@ if __name__ == "__main__":
     input_param = RockCombstAnly.RT(inst).input_param
     
     result = Main(ex_df, func_cstr, func_gamma, input_param)
+    result.do_iterat(maxiter=20)
+    plt.plot(ex_df.index,result.of_init)
+    
+    t = 0.005
+    eta = 1.0
+    of_range = np.arange(-10, 50, 0.1)
+    plt.rcParams["font.family"] = "Times New Roman"
+    plt.rcParams["font.size"] = 13
+    plt.rcParams["text.usetex"] = False
+    
+    left_eq14 = np.array([result.func_left_eq14(of,t,eta) for of in of_range])
+    right_eq14 = np.array([result.func_right_eq14(t) for of in of_range])
+    plt.plot(of_range, left_eq14, label="Left-hand")
+    plt.plot(of_range, right_eq14, label="Right-hand")
+    plt.ylabel(r"Left and right hand side of Eq.14 [m/s]")
+    plt.xlabel(r"O/F [] $M_f$")
+    plt.legend()
+    
+    
+    error = np.array([result.func_error_of(of, t, eta) for of in of_range])
+    plt.plot(of_range, error)
+    plt.ylabel("Error")
+    plt.xlabel("O/F")
+
+    of_cal =  np.array([result.func_of(of, t, eta) for of in of_range])
+    plt.plot(of_range, of_cal)
+    plt.plot(of_range, of_range)
+    plt.ylabel("O/F cal")
+    plt.xlabel("O/F")
+    plt.ylim(-5, 5)
+
+    Pc = ex_df.Pc[t]
+    mox = ex_df.mox[t]
+    At = np.pi*np.power(input_param["Dt"], 2)/4
+    func_denom = lambda of: Pc*At - mox*eta*func_cstr(of, Pc*1.0e-6)[0]
+    denom = np.array([func_denom(of) for of in of_range])
+    plt.plot(of_range, denom)
+    plt.ylabel("Denominator of O/F")
+    plt.xlabel("O/F")
+    
+    func_nume = lambda of: eta*mox*func_cstr(of, Pc*1.0e-6)[0]
+    nume = np.array([func_nume(of) for of in of_range])
+    plt.plot(of_range, nume)
+    plt.ylabel("Denominator of O/F")
+    plt.xlabel("O/F")
+    
+    plt.plot(of_range, nume/denom)
+    plt.ylabel("Claculated O/F")
+    plt.xlabel("O/F")
+    
+
+    diff = of_cal-of_range
+    plt.plot(of_range, diff, "ko")
+    plt.ylabel("Difference of O/F")
+    plt.xlabel("O/F cal")
+    plt.ylim(-10, 10)
+    
+    error_of = diff/of_cal
+    plt.plot(of_range, error_of)
+    plt.ylabel("Error")
+    plt.xlabel("Difference of O/F")
 #    plt.plot(result.index, result.of)
