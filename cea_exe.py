fix: fix to finish calculation if the O/F is exceed the O/F iteration boundary at O/F iteration

cea_exe.py
@@ -124,6 +124,11 @@ class CEA_execute:
         os.chdir("..")
         return
 
+    def onetime_exe(self):
+        """
+        Execute CEA in onetime; preparing ".inp", execute CEA and read ".out" file
+        """
+        pass
        
     def all_exe(self):
         """
@@ -201,6 +206,7 @@ class CEA_execute:
         self._csv_out_(dbfld_path, of, Pc, value_rock, point="") #write out in csv-file
             
         return(of, Pc, value_c, value_t, value_e, value_rock)
+
 
 class Read_output:
     """ Read contens in one ".out" file
@@ -360,238 +366,6 @@ class Read_output:
     #    Isp = rock_ntpl(t=rock_param["Isp"][0], e=rock_param["Isp"][1])    
     
         return(cond_param, therm_param, trans_param, rock_param)
-
-
-
-class Read_datset:
-    """
-    Read_datset(fld_path, fexten="csv")
-    
-    Class to read and interpolate datasets calculated parameter with respect to every O/F and Pc
-    
-    Parameter
-    ---------
-    self.fld_path: string
-        folder path containing dataset files
-
-    self.fexten: string
-        Defalut-value = "csv".
-        Extension of dataset file. Default is CSV file.
-
-    Class variable
-    --------
-    self.of: ndarray
-        oxidizer to fuel ratio
-    
-    self.Pc: ndarray
-        chamber pressure [MPa]
-
-    """
-    
-    def __init__(self, fld_path, fexten="csv"):
-        self.fld_path = fld_path
-        self.fexten = fexten
-        if os.path.exists(self.fld_path):
-            flist = self.get_flist()
-#            print(flist)
-            init_fpath = os.path.join(self.fld_path, flist[0] +"."+self.fexten)
-            dataframe = pd.read_csv(init_fpath, header=0, index_col=0, comment="#")
-            self.of = np.asarray([float(i) for i in dataframe.index])
-            self.Pc = np.asarray([float(i) for i in dataframe.columns])
-        else:
-            print("There is no such a dataset file/n{}".format(self.fld_path))
-    
-    def _read_csv_(self, param_name):
-        """
-        Read a csv-type-dataset files
-        
-        Parameter
-        ---------
-        param_name: string
-            Parameter name which is a dataset file name \n
-            e.g. "CSTAR", "GAMMAs", "T_c", "Cp_c"
-        
-        Return
-        ------
-        array: 2-ndarray, array.shape -> (of.size(), Pc.size())
-            values of a calculated parameter with respect to every O/F and Pc
-        """
-        fpath = os.path.join(self.fld_path, param_name+"."+self.fexten)
-        
-        if os.path.exists(fpath):
-            dataframe = pd.read_csv(fpath, header=0, index_col=0, comment="#")
-            array = np.asarray(dataframe)
-        else:
-            print("There is no such a parameter \"{}\"\n".format(param_name))
-            print("Please select a parameter from below list\n")
-            print(self.get_flist())
-            sys.exit(1)
-        return(array)
-
-    def get_flist(self):
-        """
-        Get dataset files list
-        
-        Return
-        -------
-        file_list: list
-            list of csv files
-        """
-        split =  lambda r: os.path.splitext(r)[0] # get file name without extention
-        file_list = [os.path.basename(split(r))  for r in glob.glob(self.fld_path + "/*.{}".format(self.fexten))]
-        return(file_list)
-
-    def gen_func(self, param_name, extraporate="linear"):
-        """
-        Generate function of calculated parameter with respect to O/F and Pc.
-        Return a value after reading csv file and interpolate the data.
-        
-        Parameter
-        ---------
-        param_name: string
-            Parameter name which is a dataset file name \n
-            e.g. "CSTAR", "GAMMAs", "T_c", "Cp_c"
-        
-        extraporate: string; optional
-            "exp": using f(x)=theta*numpy.exp(x-p)+q to extraporate the region out of database
-            "exp2"
-            "ln"
-            "inverse"
-            "power"
-            "linear" Default; using linear function to extraporate
-
-        Return
-        ------
-        func: function(of, Pc)
-            A function which return a interpolated value (array-like)
-        """
-        array = self._read_csv_(param_name)
-        func_interp = interpolate.interp2d(self.of, self.Pc, array.T, kind="cubic", bounds_error=False)
-        def func(of, Pc):
-            """Function to do interpolation and linear-extrapolation about the assigned database.
-            Extrapolation is avalable only when assigned O/F is out of data-base range
-            
-            Parameter
-            -----------
-            of: float,
-                O/F
-            Pc: float,
-                Chamber Pressure [Pa]
-            
-            Return
-            ----------
-            val: float
-                interpolated or extrapolated value
-            """
-            Pc = Pc*1.0e-6
-            cstr_array = func_interp(self.of, Pc)
-            def extrapfunc_exp(of, a, b,diff, ddiff):
-                theta = ddiff/diff
-                p = np.log(diff/theta)/theta + a
-                q = b - np.exp(theta*(a-p))
-                val = np.exp(theta*(a-p))+q
-                return(val)
-            def extrapfunc_exp2(of, a,b, diff, ddiff):
-                p = a - ddiff/diff
-                theta = np.power(diff,2)/(ddiff*np.exp(ddiff/diff))
-                q = b - theta*np.exp(a-p)
-                val = theta*np.exp(a-p) + q
-                return(val)
-            def extrapfunc_ln(of, a,b, diff, ddiff):
-                p = diff/ddiff + a
-                theta = -np.power(diff, 2.0)/ddiff
-                q = b - theta*np.log(a-p)
-                val = theta*np.log(of - p) + q
-                return(val)
-            def extrapfunc_inverse(of, a,b, diff, ddiff):
-                p = 2*diff/ddiff + a
-                theta = -diff*np.power(a-p, 2.0)
-                q = b - theta/(a-p)
-                val = theta/(of-p) + q
-                return(val)
-            def extrapfunc_power(of, a, b, diff, ddiff, dddiff):
-                phi = (dddiff*diff-2*np.power(ddiff,2))/(dddiff*diff-np.power(ddiff,2))
-                p = a - (phi-1)*diff/ddiff
-                theta = diff/(phi*np.power(a-p,phi-1))
-                q = b - theta*np.power(a-p,phi)
-                val = theta*np.power(of-p,phi)+q
-                return(val)
-            def extrapfunc_linear(of, a, b, diff):
-                val= diff*(of-a) + b
-                return(val)
-            
-            if of<self.of.min(): #when assigned O/F is smaller than minimum O/F of database
-                diff_begin = (-3*cstr_array[0] +4*cstr_array[1] -cstr_array[2])/(2*(self.of[1]-self.of[0]))
-                ddiff_begin = (2*cstr_array[0] -5*cstr_array[1] + 4*cstr_array[2] -cstr_array[3])/np.power((self.of[1]-self.of[0]),2.0)
-#                dddiff_begin = 
-                a = self.of.min()
-                b = cstr_array[0]
-                if extraporate=="exp":
-                    val = extrapfunc_exp(of, a, b, diff_begin, ddiff_begin)
-                elif extraporate=="exp2":
-                    val = extrapfunc_exp2(of, a, b, diff_begin, ddiff_begin)
-                elif extraporate=="ln":
-                    val = extrapfunc_ln(of, a, b, diff_begin, ddiff_begin)
-                elif extraporate=="inverse":
-                    val = extrapfunc_inverse(of, a, b, diff_begin, ddiff_begin)
-                elif extrapfunc_power=="power":
-                    val = extrapfunc_power(of, a, b, diff_begin, ddiff_begin, dddiff_begin)
-                elif extraporate=="linear":
-                    val = extrapfunc_linear(of, a, b, diff_begin)
-            elif self.of.max()<of: #when assigned O/F is larger than maximum O/F of database
-                diff_end = (cstr_array[len(cstr_array)-3] -4* cstr_array[len(cstr_array)-2] +3*cstr_array[len(cstr_array)-1])/(2*(self.of[len(self.of)-1]-self.of[len(self.of)-2]))
-                ddiff_end = (-2*cstr_array[len(cstr_array)-4] +4*cstr_array[len(cstr_array)-3] -5*cstr_array[len(cstr_array)-2] +2*cstr_array[len(cstr_array)-1])/np.power(self.of[len(self.of)-1]-self.of[len(self.of)-2], 2.0)
-#                dddiff_end = 
-                a = self.of.max()
-                b = cstr_array[len(cstr_array)-1]
-                if extraporate=="exp":
-                    val = extrapfunc_exp(of, a, b, diff_end, ddiff_end)
-                elif extraporate=="exp2":
-                    val = extrapfunc_exp2(of, a, b, diff_end, ddiff_end)
-                elif extraporate=="ln":
-                    val = extrapfunc_ln(of, a, b, diff_end, ddiff_end)
-                elif extraporate=="inverse":
-                    val = extrapfunc_inverse(of, a, b, diff_end, ddiff_end)
-                elif extrapfunc_power=="power":
-                    val = extrapfunc_power(of, a, b, diff_end, ddiff_end, dddiff_end)
-                elif extraporate=="linear":
-                    val = extrapfunc_linear(of, a, b, diff_end)
-            else: #when assigned O/F is with in the range of O/F
-                    val = func_interp(of, Pc)[0]
-            return(val)
-        
-        return(func)
-        
-    def plot(self, param_name, pickup_num):    
-        """
-        Plot graph about relationship of param to of and Pc
-        
-        Parameters
-        ----------
-        param_name: string
-            Parameter name which is a dataset file name \n
-            e.g. "CSTAR", "GAMMAs", "T_c", "Cp_c"
-
-        pickup_num: int
-            The number of "Pc" picked up to draw a graph 
-        """
-        if pickup_num > len(self.Pc):
-            pass
-        else:
-            Pc_nlm = (self.Pc[-1]-self.Pc[0])/(pickup_num-1)
-            pick_idx = lambda i: np.abs(np.asarray(self.Pc)-(Pc_nlm*i + self.Pc[0])).argmin()
-            idx = [pick_idx(i) for i in range(pickup_num)]
-
-        array = self._read_csv_(param_name)        
-        plt.rcParams["font.family"] = "Times New Roman"
-        plt.rcParams["font.size"] = 17
-        fig = plt.figure(figsize=(8,6))
-        ax = fig.add_subplot(111)
-        for i in idx:
-            ax.plot(self.of, array[:,i], label=r"$P_c$ = {} MPa".format(self.Pc[i]))
-        ax.legend(loc="best", fontsize=16)
-        ax.set_xlabel(r"$O/F$")
-        ax.set_ylabel("${}$".format(param_name))
 
 
 if __name__ == "__main__":

cea_post.py
@@ -132,6 +132,18 @@ class Read_datset:
             """
             Pc = Pc*1.0e-6
             cstr_array = func_interp(self.of, Pc)
+            def cea_exe(of, Pc):
+                """ Using single execute of CEA instead of not using extraporation 
+                
+                Parameter
+                --------
+                of: float
+                    O/F
+                Pc: float
+                    chamber pressure [MPa]
+                """
+                pass
+            
             def extrapfunc_exp(of, a, b,diff, ddiff):
                 theta = ddiff/diff
                 p = np.log(diff/theta)/theta + a
@@ -173,7 +185,9 @@ class Read_datset:
 #                dddiff_begin = 
                 a = self.of.min()
                 b = cstr_array[0]
-                if extraporate=="exp":
+                if extraporate == False:
+                    val = cea_exe(of, Pc)
+                elif extraporate=="exp":
                     val = extrapfunc_exp(of, a, b, diff_begin, ddiff_begin)
                 elif extraporate=="exp2":
                     val = extrapfunc_exp2(of, a, b, diff_begin, ddiff_begin)
@@ -191,7 +205,9 @@ class Read_datset:
 #                dddiff_end = 
                 a = self.of.max()
                 b = cstr_array[len(cstr_array)-1]
-                if extraporate=="exp":
+                if extraporate == False:
+                    val = cea_exe(of, Pc)
+                elif extraporate=="exp":
                     val = extrapfunc_exp(of, a, b, diff_end, ddiff_end)
                 elif extraporate=="exp2":
                     val = extrapfunc_exp2(of, a, b, diff_end, ddiff_end)

rt_3.py
@@ -131,10 +131,15 @@ class Main:
         Ae = np.power(self.input_param["Dt"], 2)*np.pi/4 * eps
         self.of_init = self.of_init.where(self.of_init>0, other=1.0e-2)
         for i in tqdm(self.ex_df.index):
+            of_bound_max = 1.0e+3 # maximum value of O/F boundary at O/F iteration 
             try:
                 tmp = optimize.newton(self.func_error_eq9,  self.of_init[i], maxiter=100, tol=1.0e-3, args=(i, lmbd, eps, Ae))
             except:
-                tmp = optimize.brentq(self.func_error_eq9, 1.0e-2, 1000, maxiter=100, xtol=1.0e-3, args=(i, lmbd, eps, Ae))
+                try:
+                    tmp = optimize.brentq(self.func_error_eq9, 1.0e-2, of_bound_max, maxiter=100, xtol=1.0e-3, args=(i, lmbd, eps, Ae))
+                except ValueError:
+                    # if O/F exceeds of_bound_max and stop at optimization, maximum value of O/F boundary is assined as O/F
+                    tmp = of_bound_max
             self.of = np.append(self.of, tmp)
             j +=1
         self.anl_df["of"] = self.of
@@ -270,14 +275,14 @@ def func_Ve(of, Pc, eps, func_cstr, func_gamma):
 if __name__ == "__main__":
 #    import sys
 #    sys.path.append(os.getcwd())
-    import RockCombstAnly
-    inst = RockCombstAnly.Cui_input()
-    db_of = RockCombstAnly.RT(inst).of
-    db_Pc = RockCombstAnly.RT(inst).Pc
-    ex_df = RockCombstAnly.RT(inst).ex_df
-    func_cstr = RockCombstAnly.RT(inst).cstr
-    func_gamma = RockCombstAnly.RT(inst).gamma
-    input_param = RockCombstAnly.RT(inst).input_param
+    import RockCombstAnly_cui
+    inst = RockCombstAnly_cui.Cui_input()
+    db_of = RockCombstAnly_cui.RT(inst).of
+    db_Pc = RockCombstAnly_cui.RT(inst).Pc
+    ex_df = RockCombstAnly_cui.RT(inst).ex_df
+    func_cstr = RockCombstAnly_cui.RT(inst).cstr
+    func_gamma = RockCombstAnly_cui.RT(inst).gamma
+    input_param = RockCombstAnly_cui.RT(inst).input_param
     
     result = Main(ex_df, func_cstr, func_gamma, input_param)
     df = result.execute_RT()