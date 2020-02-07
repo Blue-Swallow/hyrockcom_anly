6ad46e1a8943b680b811cee82372492352da3719
Blue-Swarrow <ayumu.colossians2.6@gmail.com>
2020-02-07 09:10:47 +0900

    add: RT-2 patch mode

--- HyRockCom_Anly_cui.py
+++ HyRockCom_Anly_cui.py
@@ -24,6 +24,7 @@ from rt import rt_4_error
 from rt import rt_5
 from rt import rt_5_error
 from rt import rt_2_hybrid
+from rt import rt_2_patch
 import matplotlib.pyplot as plt
 
 
@@ -102,8 +103,15 @@ class RT():
             if self.input_param["mode_error"] == "y":
                 # anl_df = self.error_cal_rt5(anl_df)
                 anl_df = rt_5_error.main(anl_df, self.ex_df, self.input_param, self.cstr, self.gamma)
+
         if self.input_param["mode"] == 6:
             anl_df = rt_2_hybrid.Main(self.ex_df, self.cstr, self.gamma, self.input_param).execute_RT(filter_level=self.input_param["filterlv"])
+            # if self.input_param["mode_error"] == "y":
+                # anl_df = self.error_cal_rt5(anl_df)
+                # anl_df = rt_5_error.main(anl_df, self.ex_df, self.input_param, self.cstr, self.gamma)
+
+        if self.input_param["mode"] == 7:
+            anl_df = rt_2_patch.Main(self.ex_df, self.cstr, self.gamma, self.input_param).execute_RT(filter_level=self.input_param["filterlv"])
             # if self.input_param["mode_error"] == "y":
                 # anl_df = self.error_cal_rt5(anl_df)
                 # anl_df = rt_5_error.main(anl_df, self.ex_df, self.input_param, self.cstr, self.gamma)
@@ -189,7 +197,7 @@ class Cui_input():
             self.cea_path = os.path.join(cadir, "cea_db_maker", "cea_db", foldername, "csv_database")
         else:
             self._select_mode_()
-            if self.input_param["mode"] == 6:
+            if (self.input_param["mode"] == 6) or (self.input_param["mode"] == 7):
                 self._input_filterlv_()
             self._input_nozzle_()
             self._input_eps_()
@@ -325,13 +333,15 @@ class Cui_input():
                     3: "RT-3",
                     4: "RT-4",
                     5: "RT-5",
-                    6: "RT-2 Hybrid"}
+                    6: "RT-2 Hybrid",
+                    7: "RT-2 Patch"}
             inp = int(input(" 1: RT-1; assuming c* is constant\n"+\
                   " 2: RT-2; assuming c* efficiency is constant\n"+\
                   " 3: RT-3; assuming nozzle discharge coefficient is constant; lambda1\n"+\
                   " 4: RT-4; assuming thrust deduction coefficnet is constant; lambda2\n"+\
                   " 5: RT-5; assuming constant c* efficiency and using O/F calculated by RT-1 at c* calculation\n"+\
-                  " 6: RT-2 Hybrid; hybrid method which swicth RT-2 to RT-5 at multi solution region\n>>"))
+                  " 6: RT-2 Hybrid; hybrid method which swicth RT-2 to RT-5 at multi solution region (beta)\n"+\
+                  " 7: RT-2 Patch; enable patch mode at multi solution region (beta)\n>>"))
             if inp in mode.keys():
                 self.input_param["mode"] = inp
                 break
--- rt/rt_2_patch.py
+++ rt/rt_2_patch.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Apr 21 18:03:51 2018
+This program is moddule for RT-2 patch
 
-@author: T.J.-LAB-PC
+@author: Blue-Swallow
 """
 
 import numpy as np
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 from scipy import integrate
 from scipy import optimize
 from . import rt_1
+# from . import rt_5
 from tqdm import tqdm
 import os
 
@@ -22,16 +23,18 @@ class Main:
         self.func_gamma = func_gamma
         self.input_param = input_param
         self.plot = plot
-        self.of_rt1 = rt_1.Main(self.ex_df, self.input_param).of
+        # self.of_rt1 = rt_1.Main(self.ex_df, self.input_param).of
+        # self.of_init = rt_5.Main(self.ex_df, self.func_cstr, self.func_gamma, self.input_param).execute_RT().of
+        self.of_init = rt_1.Main(self.ex_df, self.input_param).of
         self.anl_df = pd.DataFrame([], index=self.ex_df.index)
         self.counter_eta_iterat = 0
         
     def execute_RT(self, maxiter=30, eta_init=0.8, filter_level=10.0):
-        dx = 1.0e-3 # dO/F
-        x_init = 1.0 # initial x when newton iteration is conducted.
-        x_min = 1.0e-2
-        x_max = 1.0e+2
-        dydx = lambda x: (self.func_left_eq14_ms(x+dx) - self.func_left_eq14_ms(x-dx))/(2*dx) # delivative of the left hand side of Eq.14
+        # dx = 1.0e-3 # dO/F
+        # x_init = 1.0 # initial x when newton iteration is conducted.
+        # self.x_min = 1.0e-2
+        # self.x_max = 1.0e+2
+        # dydx = lambda x: (self.func_left_eq14_ms(x+dx) - self.func_left_eq14_ms(x-dx))/(2*dx) # delivative of the left hand side of Eq.14
         """ plot part to confirm the shape of dydx """
         # x_array = np.arange(0.1, 3.0, 1e-2)
         # plt.plot(x_array, np.array([dydx(x) for x in x_array]))
@@ -41,23 +44,24 @@ class Main:
         # plt.xlabel(r"$O/F$ [-]")
         # plt.ylabel(r"$ \frac{d}{dO/F} c^*(1+ \frac{1}{O/F})$")
         # plt.savefig("dydx.png", dpi=400)
-        xx_0 = optimize.newton(dydx, x_init) # seek the solution of dydx; namely the peak of concave and convex of LHS of Eq.14
-        try:
-            xx_1 = optimize.brentq(dydx, x_min, xx_0 -dx)
-        except ValueError:
-            xx_1 = xx_0
-        if round(xx_0, 5) == round(xx_1, 5):
-            xx_1 = optimize.brentq(dydx, xx_0 +dx, x_max)
-        if xx_0 > xx_1:
-            xx_tmp = xx_0
-            xx_0 = xx_1
-            xx_1 = xx_tmp
-        self.filter_level = filter_level
-        self.ms_upper_end = self.func_left_eq14_ms(xx_1)
-        self.ms_bottom_end = self.func_left_eq14_ms(xx_0)
-        self.ms_left_end = self.iterat_newton_of_ms(self.ms_upper_end, x_min, xx_0)
-        self.ms_right_end = self.iterat_newton_of_ms(self.ms_bottom_end, xx_1, x_max)
+        # self.xx_0 = optimize.newton(dydx, x_init) # seek the solution of dydx; namely the peak of concave and convex of LHS of Eq.14
+        # try:
+        #     self.xx_1 = optimize.brentq(dydx, self.x_min, self.xx_0 -dx)
+        # except ValueError:
+        #     self.xx_1 = self.xx_0
+        # if round(self.xx_0, 5) == round(self.xx_1, 5):
+        #     self.xx_1 = optimize.brentq(dydx, self.xx_0 +dx, self.x_max)
+        # if self.xx_0 > self.xx_1:
+        #     xx_tmp = self.xx_0
+        #     self.xx_0 = self.xx_1
+        #     self.xx_1 = xx_tmp
+        # self.filter_level = filter_level
+        # self.ms_upper_end = self.func_left_eq14_ms(self.xx_1)
+        # self.ms_bottom_end = self.func_left_eq14_ms(self.xx_0)
+        # self.ms_left_end = self.iterat_newton_of_ms(self.ms_upper_end, self.x_min, self.xx_0)
+        # self.ms_right_end = self.iterat_newton_of_ms(self.ms_bottom_end, self.xx_1, self.x_max)
 
+        self.filter_level = filter_level
         try:
             self.iterat_newton_eta(maxiter, eta_init=eta_init)
         except:
@@ -195,37 +199,61 @@ class Main:
             string = "th"
         print("{}".format(self.counter_eta_iterat)+ string + " iteration")
         print("eta = {}".format(eta))
-        filter_center = eta*(self.ms_upper_end + self.ms_bottom_end)/2
-        filter_width = (self.ms_upper_end - self.ms_bottom_end)*self.filter_level
-        filter_upper_end = filter_center + filter_width/2
-        filter_bottom_end = filter_center - filter_width/2
+
+        dx = 1.0e-3 # dO/F
+        x_init = 1.0 # initial x when newton iteration is conducted.
+        x_min = 1.0e-2
+        x_max = 1.0e+2
+        dydx = lambda x: (self.func_left_eq14_ms(x+dx, eta) - self.func_left_eq14_ms(x-dx, eta))/(2*dx) # delivative of the left hand side of Eq.14
+        xx_0 = optimize.newton(dydx, x_init) # seek the solution of dydx; namely the peak of concave and convex of LHS of Eq.14
+        try:
+            xx_1 = optimize.brentq(dydx, x_min, xx_0 -dx)
+        except ValueError:
+            xx_1 = xx_0
+        if round(xx_0, 5) == round(xx_1, 5):
+            xx_1 = optimize.brentq(dydx, xx_0 +dx, x_max)
+        if xx_0 > xx_1:
+            xx_tmp = xx_0
+            xx_0 = xx_1
+            xx_1 = xx_tmp
+        ms_upper_end = self.func_left_eq14_ms(xx_1, eta) # multisolution region upper end
+        ms_bottom_end = self.func_left_eq14_ms(xx_0, eta) # multisolution region bottom end
+        ms_left_end = self.iterat_newton_of_ms(ms_upper_end, x_min, xx_0, eta)
+        ms_right_end = self.iterat_newton_of_ms(ms_bottom_end, xx_1, x_max, eta)
+        filter_center = (ms_upper_end + ms_bottom_end)/2
+        filter_width = (ms_upper_end - ms_bottom_end)*self.filter_level
+        self.filter_upper_end = filter_center + filter_width/2
+        self.filter_bottom_end = filter_center - filter_width/2
+        self.filter_left_end = self.iterat_newton_of_ms(self.filter_upper_end, x_min, xx_0, eta)
+        self.filter_right_end = self.iterat_newton_of_ms(self.filter_bottom_end, xx_1, x_max, eta)
+
         for i in tqdm(self.ex_df.index):
             of_bound_max = 1.0e+3 # maximum value of O/F boundary at O/F iteration 
-            of_init = self.of_rt1.where(self.of_rt1>0, other=of_bound_max)
+            of_init = self.of_init.where(self.of_init>0, other=of_bound_max)
             self.of_init = of_init
             rhs = self.ex_df.Pc[i]*(np.pi*np.power(self.input_param["Dt"], 2)/4)/self.ex_df.mox[i]
             try:
-                if (filter_upper_end > rhs) and (rhs > filter_bottom_end): # using RT-5
-                    tmp = optimize.newton(self.func_error_eq14_rt5, of_init[i], maxiter=100, tol=1.0e-5, args=(i, eta))
-                    switch_tmp = "RT-5"
+                if (self.filter_upper_end > rhs) and (rhs > self.filter_bottom_end): # using Patch
+                    tmp = optimize.newton(self.func_error_eq14_patch, of_init[i], maxiter=100, tol=1.0e-5, args=(i, eta))
+                    switch_tmp = "ON"
                 else: # using RT-2
-                    tmp = optimize.newton(self.func_error_eq14_rt2, of_init[i], maxiter=100, tol=1.0e-5, args=(i, eta))
-                    switch_tmp = "RT-2"
+                    tmp = optimize.newton(self.func_error_eq14, of_init[i], maxiter=100, tol=1.0e-5, args=(i, eta))
+                    switch_tmp = "OFF"
             except:
                 try:
-                    if (filter_upper_end > rhs) and (rhs > filter_bottom_end): # using RT-5         
-                        tmp = optimize.brentq(self.func_error_eq14_rt5, 1.0e-3, of_bound_max, maxiter=100, xtol=1.0e-5, args=(i, eta))
-                        switch_tmp = "RT-5"
+                    if (self.filter_upper_end > rhs) and (rhs > self.filter_bottom_end): # using Patch         
+                        tmp = optimize.brentq(self.func_error_eq14_patch, 1.0e-3, of_bound_max, maxiter=100, xtol=1.0e-5, args=(i, eta))
+                        switch_tmp = "ON"
                     else: # using RT-2
-                        tmp = optimize.brentq(self.func_error_eq14_rt2, 1.0e-3, of_bound_max, maxiter=100, xtol=1.0e-5, args=(i, eta))
-                        switch_tmp = "RT-2"
+                        tmp = optimize.brentq(self.func_error_eq14, 1.0e-3, of_bound_max, maxiter=100, xtol=1.0e-5, args=(i, eta))
+                        switch_tmp = "OFF"
                 except ValueError:
                     tmp = of_bound_max
             of = np.append(of, tmp)
             switch = np.append(switch, switch_tmp)
             j +=1
         self.anl_df["of"] = of
-        self.anl_df["switch"] = switch
+        self.anl_df["patch"] = switch
         if self.plot:
             plt.plot(self.anl_df.of, label="{} iteration".format(self.counter_eta_iterat))
             plt.xlabel("Time [s]")
@@ -236,7 +264,7 @@ class Main:
         return(of)
 
     
-    def iterat_newton_of_ms(self, rhs, x_left, x_right):
+    def iterat_newton_of_ms(self, rhs, x_left, x_right, eta):
         """
         Function to calculate O/F. This function is specified to be applid to multi-solution patch.
         
@@ -255,38 +283,38 @@ class Main:
             O/F, solution of c*(1+1/O/F) - R.H.S. = 0
         """
         warnings.filterwarnings("error")
-        of = optimize.brentq(self.func_error_eq14_ms, x_left, x_right, maxiter=100, xtol=1.0e-5, args=(rhs,))
+        of = optimize.brentq(self.func_error_eq14_ms, x_left, x_right, maxiter=100, xtol=1.0e-5, args=(rhs,eta))
         warnings.resetwarnings()
         return(of)
     
-    def func_error_eq14_rt2(self, of, t, eta):
+    def func_error_eq14(self, of, t, eta):
         """ Return the error of Eq.14
         """
-        left_eq14 = self.func_left_eq14_rt2(of,t,eta)
+        left_eq14 = self.func_left_eq14(of,t,eta)
         right_eq14 = self.func_right_eq14(t)
         diff = left_eq14 - right_eq14
         error = diff/right_eq14
         return(error)
 
-    def func_error_eq14_rt5(self, of, t, eta):
+    def func_error_eq14_patch(self, of, t, eta):
         """ Return the error of Eq.14
         """
-        left_eq14 = self.func_left_eq14_rt5(of,t,eta)
+        left_eq14 = self.func_left_eq14_patch(of,t,eta)
         right_eq14 = self.func_right_eq14(t)
         diff = left_eq14 - right_eq14
         error = diff/right_eq14
         return(error)
 
-    def func_error_eq14_ms(self, of, rhs):
+    def func_error_eq14_ms(self, of, rhs, eta):
         """ Return the error of Eq.14
         """
-        left_eq14 = self.func_left_eq14_ms(of)
+        left_eq14 = self.func_left_eq14_ms(of, eta)
         right_eq14 = rhs
         diff = left_eq14 - right_eq14
         error = diff/right_eq14
         return(error)
 
-    def func_left_eq14_rt2(self, of, t, eta):
+    def func_left_eq14(self, of, t, eta):
         """
         Left-hand side value of Eq.(14), Nagata et.al., "Evaluations of Data Reduction Methods for Hybrid Rockets", 65th IAC, 2014.
         
@@ -300,7 +328,7 @@ class Main:
         left_val = eta*cstr*(1 + 1/of)
         return(left_val)
 
-    def func_left_eq14_rt5(self, of, t, eta):
+    def func_left_eq14_patch(self, of, t, eta):
         """
         Left-hand side value of Eq.(14), Nagata et.al., "Evaluations of Data Reduction Methods for Hybrid Rockets", 65th IAC, 2014.
         
@@ -309,12 +337,12 @@ class Main:
         left_val: float
             eta *cstr_th *(1+1/(O/F)) 
         """
-        Pc = self.ex_df.Pc[t]
-        cstr = self.func_cstr(self.of_init[t], Pc)
-        left_val = eta*cstr*(1 + 1/of)
+        inclination = (self.filter_bottom_end - self.filter_upper_end)/(self.filter_right_end - self.filter_left_end)
+        section = self.filter_upper_end - inclination
+        left_val = inclination *of + section
         return(left_val)
 
-    def func_left_eq14_ms(self, of):
+    def func_left_eq14_ms(self, of, eta):
         """
         Left-hand side value of Eq.(14), Nagata et.al., "Evaluations of Data Reduction Methods for Hybrid Rockets", 65th IAC, 2014.
         This function is specified to be applid to multi-solution patch.
@@ -326,7 +354,7 @@ class Main:
         """
         Pc = self.ex_df["Pc"].mean() #[Pa] # temporary Pc which is used to calculate delivative of c*(1+1/O/F). Pc does not affect the shape of cstr
         cstr = self.func_cstr(of, Pc)
-        left_val = cstr*(1 + 1/of)
+        left_val = eta*cstr*(1 + 1/of)
         return(left_val)
 
     def func_right_eq14(self, t):
